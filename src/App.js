8662093eb6a81a51c5317d5e2589732250ea548f
yjw8459 <ryu01410@gmail.com>
2021-11-15 22:17:36 +0900

    master

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import './App.css';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 import data from './data';
+import { Link, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   let [shoes, shoes변경] = useState(data);
@@ -30,21 +32,20 @@ function App() {
         </Container>
       </Navbar>
 
-      <div style={{height:'300px', backgroundColor:'grey'}} className="background">
-        <h1>20% Season Off</h1>
-        <p>
-          YJW Shoe ShoeShop
-            age : 27
 
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </div>
+      <Route exact path="/">
+        <div style={{height:'300px', backgroundColor:'grey'}} className="background">
+          <h1>20% Season Off</h1>
+          <p>
+            YJW Shoe ShoeShop
+              age : 27
 
-
-
-      <div className="container">
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </div>
+        <div className="container">
         <div className="row">
           {
             shoes.map((shoe, i) => {
@@ -53,6 +54,25 @@ function App() {
           }
         </div>
       </div>
+      </Route>
+      <Route path="/detail">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">상품명</h4>
+              <p>상품설명</p>
+              <p>120000원</p>
+              <button className="btn btn-danger">주문하기</button> 
+            </div>
+          </div>
+        </div> 
+      </Route>
+
+
+
 
     </div>
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+//.없이 이름만 있는 경우는 라이브러리임.
+//HashRouter : 라우팅을 안전하게 할 수 있음. #뒤에 오는 것들은 절대 전송되지 않음.
+//BrowserRouter : 서버에 요청할 수도 있으므로 위험.
+
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
